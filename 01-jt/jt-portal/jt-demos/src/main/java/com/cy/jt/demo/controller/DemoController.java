a3038a2c755aacf53bb599fd34a1e09581ab504a
shushan1021 <1332192580@qq.com>
2021-06-24 16:37:13 +0800

    demo测试

--- 01-jt/jt-portal/jt-demos/src/main/java/com/cy/jt/demo/controller/DemoController.java
+++ 01-jt/jt-portal/jt-demos/src/main/java/com/cy/jt/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.cy.jt.demo.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import java.util.Arrays;
 @RestController
 @RequestMapping("/demo/")
 public class DemoController {
-    @DeleteMapping
-    public String deleteById(Integer ...id){
-        return Arrays.toString(id) + "is delete";
+    @DeleteMapping("{id}")
+    public String deleteById(@PathVariable Integer ...id){
+        return Arrays.toString(id) + " is delete";
     }
 }
